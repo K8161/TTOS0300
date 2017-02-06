1edd92e40a9d619f16c3fb930f0b416aea61c270
K8161 <K8161@student.jamk.fi>
2017-02-06 13:35:13 +0200

    T1 done

---
+++ vko6/vko6maT1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vko6maT1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int car = 0;
+        int truck = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            car++;
+            textBlock1.Text = Convert.ToString(car);
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            truck++;
+            textBlock2.Text = Convert.ToString(truck);
+        }
+    }
+}
