4fea4e1bfd9ddf6ace6586e5f766a9a91c660d43
K8161 <K8161@student.jamk.fi>
2017-02-13 13:52:28 +0200

    Finish demo

--- vko7/WPF_Demo_X3/MainWindow.xaml.cs
+++ vko7/WPF_Demo_X3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using JAMK.ICT;
 
 namespace WPF_Demo_X3
 {
@@ -20,6 +22,11 @@ namespace WPF_Demo_X3
     /// </summary>
     public partial class MainWindow : Window
     {
+        // koska useampi metodi=tapahtumakäsittelijä tulee käyttämään näitä muuttujia -->
+        //määritellään luokan tasolle luokan jäsenmuuttujiksi
+        JAMK.ICT.HockeyLeague liiga;
+        ObservableCollection<JAMK.ICT.HockeyTeam> joukkueet;
+        int counter = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,8 +42,40 @@ namespace WPF_Demo_X3
             cmbMovies.ItemsSource = muuvit;
 
             //haetaan SM-Liiga-joukkueet
-            JAMK.ICT.HockeyLeague liiga = new JAMK.ICT.HockeyLeague();
-            liiga.GetTeams();
+            liiga = new JAMK.ICT.HockeyLeague();
+            joukkueet = liiga.GetTeams();
+            cmbTeams.ItemsSource = joukkueet;
+            
+        }
+
+        private void btnBind_Click(object sender, RoutedEventArgs e)
+        {
+            //määritellään Stackpanelin DataContext
+            //Demo1: Datacontextina on olio
+            HockeyTeam tiimi = new HockeyTeam("KeuPa", "Keuruu");
+            //spRight.DataContext = tiimi;
+            //Demo2: kytketään olio-kokoelman 1. olioon
+            spRight.DataContext = joukkueet[counter];
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (counter > joukkueet.Count)
+            {
+                counter = 0;
+            }
+            counter++;
+            spRight.DataContext = joukkueet[counter];
+        }
+
+        private void btnBackward_Click(object sender, RoutedEventArgs e)
+        {
+            if (counter < 0)
+            {
+                counter = joukkueet.Count;
+            }
+            counter--;
+            spRight.DataContext = joukkueet[counter];
         }
     }
 }
