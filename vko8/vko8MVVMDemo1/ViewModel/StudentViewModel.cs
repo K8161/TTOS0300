a85b9094c784756ba3a16a9809adaee6c5610b95
K8161 <K8161@student.jamk.fi>
2017-02-20 13:14:57 +0200

    vko8demo

---
+++ vko8/vko8MVVMDemo1/ViewModel/StudentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMDemo.Model;
+using System.Collections.ObjectModel;
+
+namespace MVVMDemo.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            students.Add(new Student { FirstName = "Mark", LastName = "Allen", Asioid="A1234" });
+            students.Add(new Student { FirstName = "John", LastName = "Doe", Asioid = "B666" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Kernell", Asioid = "K2456" });
+            Students = students;
+        }
+    }
+}
